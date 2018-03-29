7b4e376aa96dcfccf99cf598924608c22289ec17
Madh93 <mhdez@protonmail.com>
2018-03-29 16:39:00 +0100

    Add logger

--- lib/oranchelo_tools.rb
+++ lib/oranchelo_tools.rb
@@ -2,8 +2,18 @@ require 'oranchelo_tools/builder'
 require 'oranchelo_tools/commands'
 require 'oranchelo_tools/version'
 
+require 'logging'
+
+# Logging root level config
+Logging.logger.root.appenders = Logging.appenders.stdout
+Logging.logger.root.level = :debug
+
 module OrancheloTools
   GEM_NAME = 'oranchelo_tools'.freeze
   PACKAGE_NAME = 'oranchelo-icon-theme'.freeze
   THEME_NAME = 'Oranchelo Icon Theme'.freeze
+
+  def self.logger
+    @logger ||= Logging.logger[self]
+  end
 end
