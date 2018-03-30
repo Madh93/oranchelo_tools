aead7f0a3231b4095b3e66f0cc3fa5a46a4ee3f7
Madh93 <mhdez@protonmail.com>
2018-03-30 15:52:10 +0100

    Utils refactoring applied in Commands module

--- lib/oranchelo_tools/commands.rb
+++ lib/oranchelo_tools/commands.rb
@@ -1,21 +1,9 @@
 require 'oranchelo_tools/commands/build'
 require 'oranchelo_tools/commands/none'
+require 'oranchelo_tools/utils'
 
 module OrancheloTools
   module Commands
-
-    def self.classes
-      @classes ||= constants.select do |c|
-        const_get(c).is_a?(Class) && (c !~ /Command/)
-      end
-    end
-
-    def self.all
-      classes.map { |c| Kernel.const_get("#{self}::#{c}") }
-    end
-
-    def self.names
-      classes.map(&:to_s).map(&:downcase)
-    end
+    extend OrancheloTools::Utils
   end
 end
--- lib/oranchelo_tools/utils.rb
+++ lib/oranchelo_tools/utils.rb
@@ -1,1 +1,24 @@
 require 'oranchelo_tools/utils/inflectable'
+
+module OrancheloTools
+  module Utils
+
+    def base_class
+      @base_class ||= name.split('::').last[0...-1]
+    end
+
+    def classes
+      @classes ||= constants.select do |c|
+        const_get(c).is_a?(Class) && (c !~ /#{base_class}/)
+      end
+    end
+
+    def all
+      classes.map { |c| Kernel.const_get("#{self}::#{c}") }
+    end
+
+    def names
+      classes.map(&:to_s).map(&:downcase)
+    end
+  end
+end
